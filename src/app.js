c4c8b9995b39bed3526100a4e0504e1047cc076f
Rob Eisenberg <rob@bluespire.com>
2015-04-22 01:18:42 -0400

    fix(all): update to new router configuration pattern

--- src/app.js
+++ src/app.js
@@ -1,18 +1,18 @@
-import {Router} from 'aurelia-router';
 import {WebAPI} from './web-api';
 
 export class App {
-  static inject = [Router, WebAPI];
-  constructor(router, api) {
-    this.router = router;
+  static inject = [WebAPI];
+  constructor(api) {
     this.api = api;
+  }
 
-    this.router.configure(config => {
-      config.title = 'Contacts';
-      config.map([
-        { route: '',              moduleId: 'no-selection',   title: 'Select'},
-        { route: 'contacts/:id',  moduleId: 'contact-detail' }
-      ]);
-    });
+  configureRouter(config, router){
+    config.title = 'Contacts';
+    config.map([
+      { route: '',              moduleId: 'no-selection',   title: 'Select'},
+      { route: 'contacts/:id',  moduleId: 'contact-detail' }
+    ]);
+
+    this.router = router;
   }
 }
