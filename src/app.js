08f5ee0791c807e54ca7775ba9184840e2e5416b
Rob Eisenberg <rob@bluespire.com>
2015-05-10 23:26:32 -0400

    fix(app): update routing config to match changes in router

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ export class App {
     config.title = 'Contacts';
     config.map([
       { route: '',              moduleId: 'no-selection',   title: 'Select'},
-      { route: 'contacts/:id',  moduleId: 'contact-detail' }
+      { route: 'contacts/:id',  moduleId: 'contact-detail', name:'contacts' }
     ]);
 
     this.router = router;
